83e6f015fbc2a8bc1414bfe3dcad80cac7776ea4
Matheus Cazon <matheuscazon@gmail.com>
2024-03-05 22:10:04 -0300

    Alterações

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
-console.log("teste");
+//Seleção de Elementos
+const generatePasswordButton = document.querySelector("#generate-password");
+const generatedPasswordElement = document.querySelector("#generated-password");
+
+// Funções
+
+// Eventos
+generatePasswordButton.addEventListener("click", () => {
+  console.log("teste");
+});
