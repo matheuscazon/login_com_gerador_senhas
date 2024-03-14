2fd20d0668d829baa59ab3d16c2a8cdf0e8fdef8
Matheus Cazon <matheuscazon@gmail.com>
2024-03-13 21:19:36 -0300

    Alterações

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,10 @@ const generatePasswordButton = document.querySelector("#generate-password");
 const generatedPasswordElement = document.querySelector("#generated-password");
 
 // Funções
+const getLetterLowerCcase = () => {
+  console.log(String.fromCharCode(55));
+};
+// Letras, Números e Símbolos
 
 // Eventos
 generatePasswordButton.addEventListener("click", () => {
